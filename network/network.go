cc769f172d85f5112438f144939a85d4cbfad08a
Josh Martin <contact@cjoshmartin.com>
2018-05-05 02:10:58 -0400

    updated

--- network/network.go
+++ network/network.go
@@ -1,29 +1,30 @@
 package network
+
 import (
 	"../layer"
 )
 
 type Network struct {
-	layers []layer.Layer
-	error   float64
-	recentAverageError float64
-recentAverageSmoothingFactor float64
+	layers                       []layer.Layer
+	error                        float64
+	recentAverageError           float64
+	recentAverageSmoothingFactor float64
 }
 
-func New(topology []uint64) Network{
+func New(topology []uint64) Network {
 	var _layers []layer.Layer
 	_numLayers := len(topology)
 
-	for layerNum := 0; layerNum < _numLayers; layerNum++{
+	for layerNum := 0; layerNum < _numLayers; layerNum++ {
 		_layers = append(_layers, layer.New())
 
-		numOutputs := topology[layerNum +1]
-		if layerNum == (len(topology) -1 ){
+		numOutputs := topology[layerNum+1]
+		if layerNum == (len(topology) - 1) {
 			numOutputs = 0
 		}
 
-		currLayer:=_layers[len(_layers)-1]
-		currLayer[len(currLayer)-1].setOutputVal(1.0);
+		currLayer := _layers[len(_layers)-1]
+		currLayer[len(currLayer)-1].setOutputVal(1.0)
 	}
-	return Network{_layers,100.0}
+	return Network{_layers, 100.0}
 }
