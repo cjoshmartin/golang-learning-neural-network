efd3de2293974312e559dbb67a1949c48977f6dc
Josh Martin <joshmartin5001@gmail.com>
2018-05-28 02:40:43 -0400

    Finish part 1

--- perceptron/perceptron_pt1/main.go
+++ perceptron/perceptron_pt1/main.go
@@ -2,17 +2,68 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 
 	"./perceptron"
-	"./training"
 )
 
+type settings struct {
+	xmin  float64
+	ymin  float64
+	xmax  float64
+	ymax  float64
+	count int64
+}
+type Line struct {
+	X1 float64
+	y1 float64
+	X2 float64
+	y2 float64
+}
+
+func f(x float64) float64 {
+	return 0.4*x + 1
+}
+func NewLine(x1, y1, x2, y2 float64) Line {
+	return Line{x1, y1, x2, y2}
+}
+
+func (currentLine *Line) UpdateLine(weights []float64) {
+	// Draw the line based on the current weights
+	// Formula is weights[0]*x + weights[1]*y + weights[2] = 0
+	*currentLine = NewLine(currentLine.X1, (-weights[2]-weights[0]*currentLine.X1)/weights[1], currentLine.X2, (-weights[2]-weights[0]*currentLine.X2)/weights[1])
+
+}
+
 func main() {
-	inputs := []float64{-1.0, 0.5}
-	test := perceptron.New(2)
+	networkSettings := settings{-400.0, -100.0, 400.0, 100.0, 0}
+	trainer := make([]perceptron.TrainingSet, 2000)
 
-	fmt.Println(test.Guess(inputs))
+	// @parms: 1) 3 inputs -- x,y, and bias
+	// @parms: 2) "Learning Constant"
+	ptron := perceptron.New(3, 0.00001)
 
-	trainings := training.New(100)
-	trainings.Show()
+	for i := range trainer { // creating random training points and calculating the "known"
+		x := (rand.Float64() * networkSettings.xmax) + networkSettings.xmin
+		y := (rand.Float64() * networkSettings.ymax) + networkSettings.ymin
+		answer := int64(1)
+		if y < f(x) {
+			answer = -1
+		}
+		trainer[i] = perceptron.TrainingSet{x, y, 1, answer}
+	}
+
+	lineOfApproximate := NewLine(networkSettings.xmin, f(networkSettings.xmin), networkSettings.xmax, f(networkSettings.xmax))
+	for true {
+		weights := ptron.GetWeight()
+
+		lineOfApproximate.UpdateLine(weights)
+		ptron.Train(trainer[networkSettings.count], trainer[networkSettings.count].Answer)
+		networkSettings.count = (networkSettings.count + 1) % int64(len(trainer))
+
+		for i := 0; i < int(networkSettings.count); i++ {
+			/*guess := */ ptron.Guess(trainer[i])
+			fmt.Println(ptron.GlobalError)
+		}
+	}
 }
--- perceptron/perceptron_pt1/perceptron/perceptron.go
+++ perceptron/perceptron_pt1/perceptron/perceptron.go
@@ -2,33 +2,61 @@ package perceptron
 
 import "math/rand"
 
-type Perceptron struct {
-	weigths []float64
+type TrainingSet struct {
+	X      float64
+	Y      float64
+	Bias   float64
+	Answer int64
 }
 
-func New(size int) Perceptron {
+type Perceptron struct {
+	weigths      []float64
+	learningRate float64
+	GlobalError  float64
+}
+
+func New(size int, _learningRate float64) Perceptron {
 	var newPerceptron Perceptron
 	newPerceptron.weigths = make([]float64, size)
 
 	for i := 0; i < len(newPerceptron.weigths); i++ {
 		newPerceptron.weigths[i] = (rand.Float64() * 2) - 1 // trying to send a random number [-1,1]
 	}
-
+	newPerceptron.learningRate = _learningRate /* setting the learnig rate, to what has been pasted */
 	return newPerceptron
 }
 
-// creates a Guess
-func (currentPerceptron *Perceptron) Guess(inputs []float64) int64 {
+//Guess will creates a Guess || feedForward algorthim
+func (currentPerceptron *Perceptron) Guess(set TrainingSet) int64 {
 	sum := 0.0
+	inputs := []float64{set.X, set.Y, set.Bias}
+
 	for i := 0; i < len(inputs); i++ {
 		sum += inputs[i] * currentPerceptron.weigths[i]
 	}
 	return signum(sum) // passing sum to activation function
 }
 
+func (currentPerceptron *Perceptron) Train(set TrainingSet, expected int64) {
+
+	algorthimGuess := currentPerceptron.Guess(set)
+	currentPerceptron.GlobalError = float64(expected - algorthimGuess)
+	inputs := []float64{set.X, set.Y, set.Bias}
+
+	for i := range currentPerceptron.weigths {
+		currentPerceptron.weigths[i] += currentPerceptron.learningRate * currentPerceptron.GlobalError * inputs[i]
+	}
+
+}
+
+/* Utls */
 func signum(n float64) int64 { // activation function
 	if n >= 0 {
 		return 1
 	}
 	return -1
+}
+
+func (currentPerceptron *Perceptron) GetWeight() []float64 {
+	return currentPerceptron.weigths
 }
